7de15a0c9b36b984b565896be308c39f7117f129
Parth Patel <parthvapi2005@gmail.com>
2024-06-06 18:27:35 -0500

    import d3 module

--- bar-chart/script.js
+++ bar-chart/script.js
@@ -1,4 +1,6 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
 fetch("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json")
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(arr => console.log(arr.data[1]))
     .catch(error => console.error(error));
