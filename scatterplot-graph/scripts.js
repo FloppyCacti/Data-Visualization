3c2055b2b2ba99052a3904743f44a3a46b61094c
Parth Patel <parthvapi2005@gmail.com>
2024-06-08 13:59:27 -0500

    add x and y scale

--- scatterplot-graph/scripts.js
+++ scatterplot-graph/scripts.js
@@ -5,7 +5,15 @@ fetch(
   .then((data) => {
     const w = 1200;
     const h = 700;
+    const padding = 50;
 
+    const xScale = d3
+      .scaleTime()
+      .domain([d3.min(data.Year), d3.max(data.Year)])
+      .range([padding, w - padding]);
+    const yScale = d3
+      .scaleTime()
+      .domain([d3.min(Math.floor(data.Seconds / 60)), d3.max(Math.floor(data.Seconds / 60))]);
     const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
   })
   .catch((err) => console.log(err));
