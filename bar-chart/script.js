9e8a08e0f78b6e4618f82ffd1accbff27587e18b
Parth Patel <parthvapi2005@gmail.com>
2024-06-07 20:58:11 -0500

    add rects to svg element

--- bar-chart/script.js
+++ bar-chart/script.js
@@ -2,13 +2,31 @@ fetch(
   "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 )
   .then((response) => response.json())
-  .then((arr) => {
-    const w = 500;
+  .then((data) => {
+    const w = 1380;
     const h = 200;
-    const body = d3
+    const padding = 40;
+    const bar_width = 4;
+
+    const dataset = data.data;
+
+    const svg = d3
       .select("body")
       .append("svg")
       .attr("width", w)
       .attr("height", h);
+
+    svg
+      .selectAll("rect")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("width", bar_width)
+      .attr("height", 100)
+      .attr("x", (d, i) => {
+        return i * (bar_width + 1);
+      })
+      .attr("y", 0)
+      .attr("fill", "blue");
   })
   .catch((error) => console.error(error));
