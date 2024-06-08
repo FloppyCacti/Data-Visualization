127903937b7d2426c04c3f28d5a0b3bf33c6ca0f
Parth Patel <parthvapi2005@gmail.com>
2024-06-07 20:35:32 -0500

    add svg element

--- bar-chart/script.js
+++ bar-chart/script.js
@@ -2,5 +2,13 @@ fetch(
   "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 )
   .then((response) => response.json())
-  .then((arr) => {})
+  .then((arr) => {
+    const w = 500;
+    const h = 200;
+    const body = d3
+      .select("body")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+  })
   .catch((error) => console.error(error));
