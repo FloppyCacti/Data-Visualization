2f51fd88127cacade6b35889da74d743cd995583
Parth Patel <parthvapi2005@gmail.com>
2024-06-09 16:53:37 -0500

    add legend

--- scatterplot-graph/scripts.js
+++ scatterplot-graph/scripts.js
@@ -6,6 +6,7 @@ fetch(
     const w = 1000;
     const h = 700;
     const padding = 50;
+    const radius = 7;
 
     const xScale = d3
       .scaleLinear()
@@ -21,6 +22,7 @@ fetch(
     const yAxis = d3.axisLeft(yScale).tickFormat((d) => {
       let minutes = Math.floor(d / 60);
       let seconds = d % 60;
+      seconds = String(seconds).padStart(2, "0");
       return minutes + ":" + seconds;
     });
     console.log(xScale(data[0].Year));
@@ -33,8 +35,15 @@ fetch(
       .append("circle")
       .attr("cx", (d) => xScale(d.Year))
       .attr("cy", (d) => yScale(d.Seconds))
-      .attr("r", 7)
-      .style("fill", "steelblue")
+      .attr("r", radius)
+      .attr("data-xvalue", (d, i) => data[i].Year)
+      .attr("data-yvalue", (d, i) => data[i].Time)
+      .style("fill", (d, i) => {
+        if (data[i].Doping == "") {
+          return "GoldenRod";
+        }
+        return "steelblue";
+      })
       .style("stroke", "black");
 
     svg
@@ -48,5 +57,35 @@ fetch(
       .attr("id", "y-axis")
       .attr("transform", "translate(" + padding + ", 0)")
       .call(yAxis);
+
+    const legendShapeSize = 17;
+    svg
+      .append("rect")
+      .attr("x", w - 200)
+      .attr("y", h - 650)
+      .attr("width", legendShapeSize)
+      .attr("height", legendShapeSize)
+      .style("fill", "GoldenRod");
+    svg
+      .append("rect")
+      .attr("x", w - 200)
+      .attr("y", h - 620)
+      .attr("width", legendShapeSize)
+      .attr("height", legendShapeSize)
+      .style("fill", "steelblue");
+    svg
+      .append("text")
+      .attr("id", "legend")
+      .attr("x", w - 195 + legendShapeSize)
+      .attr("y", h - 653 + legendShapeSize)
+      .text("No doping allegations")
+      .style("font-size", "15px");
+    svg
+      .append("text")
+      .attr("id", "legend")
+      .attr("x", w - 195 + legendShapeSize)
+      .attr("y", h - 623 + legendShapeSize)
+      .text("Doping allegations")
+      .style("font-size", "15px");
   })
   .catch((err) => console.log(err));
