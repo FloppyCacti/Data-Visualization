4b6ca24cb940d503e42f52bc8e4edf28a4b26ee9
Parth Patel <parthvapi2005@gmail.com>
2024-06-09 18:55:23 -0500

    add x-axis

--- heat-map/scripts.js
+++ heat-map/scripts.js
@@ -7,5 +7,24 @@ d3.json(
     const padding = 50;
 
     const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+    // Parse the year to create a date object
+    const parseYear = d3.timeParse("%Y");
+
+    const xScale = d3
+      .scaleTime()
+      .domain([
+        d3.min(data.monthlyVariance, (d) => parseYear(d.year)),
+        d3.max(data.monthlyVariance, (d) => parseYear(d.year)),
+      ])
+      .range([padding, w - padding]);
+
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y"));
+
+    svg
+      .append("g")
+      .attr("id", "x-axis")
+      .attr("transform", "translate(0, " + (h - padding) + ")")
+      .call(xAxis);
   })
   .catch((err) => console.log(err));
