7c85f46e345feb5b6e3c45a8dcf10a89b9b648e0
Parth Patel <parthvapi2005@gmail.com>
2024-06-10 08:29:20 -0500

    add url

---
+++ choropleth-map/script.js
@@ -0,0 +1,4 @@
+const countyURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
+const educationURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
