ebd586733cd10cf46a265db9924d896881a6de9f
Parth Patel <parthvapi2005@gmail.com>
2024-06-10 08:54:55 -0500

    add json map

--- choropleth-map/script.js
+++ choropleth-map/script.js
@@ -2,3 +2,33 @@ const countyURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
 const educationURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
+
+let svg = d3.select("#map");
+let countyData;
+let educatoinData;
+
+d3.json(countyURL).then((data, error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    countyData = topojson.feature(data, data.objects.counties).features;
+    console.log(countyData);
+
+    d3.json(educationURL).then((data, error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        educatoinData = data;
+        console.log(educatoinData);
+
+        svg
+          .selectAll("path")
+          .data(countyData)
+          .enter()
+          .append("path")
+          .attr("d", d3.geoPath())
+          .attr("class", "county");
+      }
+    });
+  }
+});
