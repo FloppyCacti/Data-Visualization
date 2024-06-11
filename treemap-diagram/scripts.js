117dbb81d5d471fa11ef498707306b2a0fa1052e
Parth Patel <parthvapi2005@gmail.com>
2024-06-11 10:19:01 -0500

    add urls

---
+++ treemap-diagram/scripts.js
@@ -0,0 +1,5 @@
+kickstarterURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
+movieURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
+videoGamesURL =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
