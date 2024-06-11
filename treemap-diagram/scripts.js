51cf3080a7b2cf58aed14209912cb0cde49968c7
Parth Patel <parthvapi2005@gmail.com>
2024-06-11 10:22:00 -0500

    fetching video game sales data

--- treemap-diagram/scripts.js
+++ treemap-diagram/scripts.js
@@ -3,3 +3,10 @@ kickstarterURL =
 movieURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
 videoGamesURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+
+fetch(videoGamesURL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.error(err));
